04162e420b2ecbcf1e907705de6938ebe4917574
Cherry Byte <adelalhaq369@gmail.com>
2024-07-24 18:39:18 +0500

    final version

---
+++ backend/addressDetails.js
@@ -0,0 +1,8 @@
+[
+    "0x0b7C7Efe2183fEf476b5f86cE53dA612c5dC89b6", // Faisal Accoutn #1
+    "0xba9D5f1D5390FA3E5fFC65F9C7332ffC40464D23", // Faisal Accoutn #2
+    "0x577Ef7C8D1DacECdbf6d45f13eDC530aeF75D3A0", //Test Account #2
+    "0xa66B34f39cFC0798fF9c6D554916cB64Ba496Ec7" //Test Account #3
+  ]
+  
+  
